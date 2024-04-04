32e8721e09984aec959359e1c3b8b7a9bfacc38b
Olexiy-repin <repinlyoha@gmail.com>
2024-04-04 13:03:36 +0300

    classwork

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+import './modal.js';
+
+// import * as math from './math';
+
+// console.log(math);
+
+// console.log(math.add(10, 20));
+// console.log(add(4, 20));
+
+// console.log(math.division(20, 2));
+
+// console.log(multiply(2, 6));
+
+// console.log(sub(23, 12));
+
+// console.log(user);
---
+++ src/js/math.js
@@ -0,0 +1,19 @@
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const division = (a, b) => {
+  return a / b;
+};
+
+export const multiply = (a, b) => {
+  return a * b;
+};
+
+export const sub = (a, b) => {
+  return a - b;
+};
+
+// export default {
+//   firstName: 'Oleksii',
+// };
