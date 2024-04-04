da843cdc4d0e8d2a01788c9f4b471e9c03328ca6
Olexiy-repin <31596886+Olexiy-repin@users.noreply.github.com>
2024-04-04 10:03:53 +0000

    Deploying to gh-pages from @ Olexiy-repin/vite-test@32e8721e09984aec959359e1c3b8b7a9bfacc38b 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,5 @@
+import{b as c}from"./assets/vendor-9b35bc79.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&s(r)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const i=c.create(`
+	<h1>Dynamic Content</h1>
+	<p>You can set the content of the lightbox with JS.</p>
+`);console.log(i);i.show();
+//# sourceMappingURL=commonHelpers.js.map
